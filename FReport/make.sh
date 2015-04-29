86b1d1cffa09983f3d6a33287aa3d2926b4dee6b
Michael Rochester <m.rochester93@googlemail.com>
2015-04-29 19:13:47 +0100

    report work

--- FReport/make.sh
+++ FReport/make.sh
@@ -1,5 +1,5 @@
 cd ~/Documents/OSProject/FReport
 inkscape -D -z --file=images/Process_Table_Diagram.svg --export-pdf=Process_Table_Diagram.pdf --export-latex
-pdflatex -interaction="batchmode" report.tex
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" | pdflatex report.tex
 bibtex report.aux
-pdflatex -interaction="batchmode" report.tex
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" | pdflatex report.tex
